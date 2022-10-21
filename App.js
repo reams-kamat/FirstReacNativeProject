7a283d8a44b90455dd91d301cd3b9fc25114ce7b
Reams kamat <reams.kamat@gmail.com>
2022-10-21 11:53:08 +0545

    till props

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import {Text,View} from 'react-native'
-
+import {Text,View,Button} from 'react-native'
+import Profile_Props_Function_Component from './components/Profile_Props_Function_Component'
+import PropsProfile from './components/Profile_Props_Class_Component'
 
 const App =()=>{
   return(
     <View>
-      <Text style={{fontSize:70}}>Hello Princess</Text>
+      <Text style={{fontSize:70}}>Props</Text>
+      <PropsProfile data={"Reams"} email="reams.kamat@gmail.com"/>
     </View>
   )
 }
